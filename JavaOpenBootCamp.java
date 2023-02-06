b80d3c23db7f7f52b8942f29162ccd171f30b3be
David Romero <davidjaime@gmail.com>
2023-02-06 16:03:59 -0400

    Programacion funcional

--- JavaOpenBootCamp.java
+++ JavaOpenBootCamp.java
@@ -2,10 +2,12 @@
 package javaopenbootcamp;
 
 public class JavaOpenBootCamp {
-
-
     public static void main(String[] args) {
-        System.out.println("hola mundo ");
+        int resultado = suma (1, 2);
+        int resultado2 = suma (1, 2);
+        System.out.println(resultado+" "+resultado2);
     }
-    
+    public static int suma(int a, int b){
+    return a + b;
+    }
 }
